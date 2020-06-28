08a21d9ab42ad1cd5f2246acfe5dcc07eb246812
nbisht30 <nbisht30@gmail.com>
2020-06-28 19:25:26 +0530

    SRP | Page Components | Result Stat

---
+++ src/main/java/com/seleniumdesign/srp/ResultStat.java
@@ -0,0 +1,27 @@
+package com.seleniumdesign.srp;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+/**
+ * @author Nikhil Bisht
+ * @date 26-06-2020
+ */
+public class ResultStat extends AbstractComponent {
+
+    @FindBy(id = "result-stats")
+    private WebElement resultStat;
+
+    public ResultStat(final WebDriver driver) { // Initialize any WebDriver
+        super(driver);
+    }
+
+    public String getStat() {
+        return this.resultStat.getText();
+    }
+
+    public boolean isDisplayed() {
+        return this.wait.until(var -> this.resultStat.isDisplayed());
+    }
+}
